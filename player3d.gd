939b62cc8552ba89b7958cf9288cf7941cc1e851
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-02-07 15:32:20 -0600

    Update 2/7

---
+++ player3d.gd
@@ -0,0 +1,44 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+const CAM_SENSITIVITY = 0.03
+@onready var camera = $Camera3D
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		self.rotate_y(-event.relative.x * (CAM_SENSITIVITY / 10.0))
+		camera.rotate_x(-event.relative.y * (CAM_SENSITIVITY / 10.0))
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+
+
+
