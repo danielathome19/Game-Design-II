ca979baf7dfd17f7be5adcecebcc2b112d610c41
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-02-09 15:27:35 -0600

    Update 2/9

--- player3d.gd
+++ player3d.gd
@@ -8,7 +8,10 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 const CAM_SENSITIVITY = 0.03
-@onready var camera = $Camera3D
+@onready var camera = $Head/Camera3D
+@onready var camera_arm = $SpringArm3D
+@onready var camera_pos = camera.position
+var first_person = true
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -35,18 +38,37 @@ func _physics_process(delta):
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
+	
+	if Input.is_action_just_pressed("change_camera"):
+		toggle_camera_parent()  # V
+	
 	move_and_slide()
 	
 
+func toggle_camera_parent():
+	var parent = "Head"
+	if first_person:
+		parent = "SpringArm3D"
+	var child = camera
+	child.get_parent().remove_child(child)
+	get_node(parent).add_child(child)
+	camera = child
+	if not first_person:
+		camera.position = camera_pos
+	first_person = not first_person
+	
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		self.rotate_y(-event.relative.x * (CAM_SENSITIVITY / 10.0))
-		camera.rotate_x(-event.relative.y * (CAM_SENSITIVITY / 10.0))
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
-
+		if first_person:
+			self.rotate_y(-event.relative.x * (CAM_SENSITIVITY / 10.0))
+			camera.rotate_x(-event.relative.y * (CAM_SENSITIVITY / 10.0))
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		else:
+			self.rotate_y(-event.relative.x * (CAM_SENSITIVITY / 10.0))
+			camera_arm.rotate_x(-event.relative.y * (CAM_SENSITIVITY / 10.0))
+			camera_arm.rotation.x = clamp(camera_arm.rotation.x, deg_to_rad(-75), deg_to_rad(75))
 
 
