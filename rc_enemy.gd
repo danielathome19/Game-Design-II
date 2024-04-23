ba763ce31e7f964ad886934b125e95f937ea91dc
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-04-23 15:00:56 -0500

    Update 4/23

--- rc_enemy.gd
+++ rc_enemy.gd
@@ -17,8 +17,28 @@ var is_stuck = false
 @onready var rayR  = $rayRight      # y=-75
 
 func _physics_process(delta):
-	pass
-
+	var target_steering = 0.0
+	update_raycasts()
+	
+	var acceleration = HORSE_POWER
+	if rayF.is_colliding():
+		var dist_to_obstacle = rayF.get_collision_point(). \
+									distance_to(self.global_transform.origin)
+		acceleration *= max(0.1, dist_to_obstacle / 10.0)
+	
+	if rayFL.is_colliding() or rayL.is_colliding():
+		target_steering -= MAX_STEER  # Steer away from obstacle
+	if rayFR.is_colliding() or rayR.is_colliding():
+		target_steering += MAX_STEER
+	
+	if is_stuck:
+		acceleration = REVERSE_POWER
+		steering = -sign(steering) * MAX_STEER
+	
+	$backLeft.engine_force = calc_engine_force(acceleration, abs($backLeft.get_rpm()))
+	$backRight.engine_force = calc_engine_force(acceleration, abs($backRight.get_rpm()))
+	check_and_right_vehicle()
+	
 
 func check_stuck_condition(delta):
 	if linear_velocity.length() < 1.0:
